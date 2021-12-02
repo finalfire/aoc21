f56a8c6377f8efcb9b19c851f7ed77fe557525a0
Francesco Cauteruccio <f.cauteruccio@gmail.com>
2021-12-02 21:49:22 +0100

    day2: single fun

--- src/day2.rs
+++ src/day2.rs
@@ -1,34 +1,15 @@
 use std::fs::*;
 
-fn part_1(input: String) -> i32 {
-    let mut depth = 0;
-    let mut hpos = 0;
+enum Part { One, Two, }
 
-    input.lines().for_each(|line| {
-        let mut it = line.split_whitespace();
-        let mode = it.next().expect("Cannot parse mode");
-        let value: i32 = it.next().unwrap().parse().expect("Cannot parse value");
-
-        match mode {
-            "forward" => hpos += value,
-            "down" => depth += value,
-            "up" => depth -= value,
-            _ => {}
-        };
-    });
-
-    depth * hpos
-}
-
-fn part_2(input: String) -> i32 {
-    let mut depth = 0;
-    let mut hpos = 0;
+fn solve(input: String, part: Part) -> i32 {
     let mut aim = 0;
-    
+    let mut depth = 0;
+    let mut hpos = 0;
+
     input.lines().for_each(|line| {
-        let mut it = line.split_whitespace();
-        let mode = it.next().expect("Cannot parse mode");
-        let value: i32 = it.next().unwrap().parse().expect("Cannot parse value");
+        let (mode, value_str) = line.split_once(' ').expect("Cannot parse row");
+        let value: i32 = value_str.parse().expect("Cannot parse value");
 
         match mode {
             "down" => aim += value,
@@ -41,13 +22,18 @@ fn part_2(input: String) -> i32 {
         };
     });
 
-    depth * hpos
+    // a single function does the trick (in part one, aim is our depth)
+    // the drawback in part 1 is that we calculate depth anyway
+    match part {
+        Part::One => aim * hpos,
+        Part::Two => depth * hpos
+    }
 }
 
 fn main() {
     let input = read_to_string("inputs/day2/input")
         .expect("Cannot read the file");
     
-    println!("{}", part_1(input.clone()));
-    println!("{}", part_2(input.clone()));
+    println!("{}", solve(input.clone(), Part::One));
+    println!("{}", solve(input.clone(), Part::Two));
 }
