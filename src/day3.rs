2f04f8869f3805c839099d26693889a1146b2971
Francesco Cauteruccio <f.cauteruccio@gmail.com>
2021-12-03 23:13:57 +0100

    day 3

--- src/day3.rs
+++ src/day3.rs
@@ -23,6 +23,66 @@ fn part_1(values: &[Vec<u8>]) -> u32 {
     gamma * ((1 << M) - gamma - 1)
 }
 
+enum Mode { OXYGEN, CO2, }
+
+fn foo(candidates: &mut [bool], i: usize, mode: Mode, values: &[Vec<u8>]) {
+    // if we have only one number, we shillin
+    if candidates.iter().filter(|c| **c).count() == 1 {
+        return;
+    }
+    
+    // check most/less common from candidates with true value
+    let (mut n0, mut n1) = (0, 0);
+    for j in 0..candidates.len() {
+        if candidates[j] {
+            match values[j][i] {
+                0 => n0 += 1,
+                1 => n1 += 1,
+                _ => {}
+            };
+        }
+    }
+
+    // bit is our target bit, mode defines the order
+    let bit = match mode {
+        Mode::OXYGEN => if n0 > n1 { 0 } else { 1 },
+        Mode::CO2 => if n0 > n1 { 1 } else { 0 }
+    };
+    
+    // update candidates
+    for j in 0..candidates.len() {
+        if candidates[j] {
+            if values[j][i] != bit {
+                candidates[j] = false;
+            }
+        }
+    }
+
+    foo(candidates, i+1, mode, values);
+}
+
+fn part_2(values: &[Vec<u8>]) -> u32 {
+    let vec2num = |v: &[u8]| -> u32 {
+        v.iter().enumerate().fold(0, |acc, (i, x)| acc + ((*x as u32) << M - i - 1))
+    };
+
+    let mut candidates = vec![true; N];
+    foo(&mut candidates, 0, Mode::OXYGEN, values);
+    let oxygen_index = candidates.iter().enumerate()
+        .find(|x| *x.1 == true)
+        .map(|x| x.0)
+        .unwrap();
+    
+    candidates = vec![true; N];
+    foo(&mut candidates, 0, Mode::CO2, values);
+    let co2_index = candidates.iter().enumerate()
+        .find(|x| *x.1 == true)
+        .map(|x| x.0)
+        .unwrap();
+    
+    vec2num(&values[oxygen_index]) * vec2num(&values[co2_index])
+}
+
 fn main() {
     let input = read_to_string("inputs/day3/input")
         .expect("Cannot read the file");
@@ -35,5 +95,7 @@ fn main() {
         })
         .collect();
 
-    println!("{}", part_1(&values));
+    //println!("{}", part_1(&values));
+    println!("{}", part_2(&values));
+
 }
